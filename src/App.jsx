1692958cf2038f1c64aa6de5285efc92c3e81160
Miguel Angel Ordóñez Pérez <m.angel091190@gmail.com>
2025-08-23 13:36:06 -0600

    Pruea sin BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SurveyApplication from './components/Survey';
-import useFetchData from './hooks/useFetchData';
+import { useEffect, useState } from "react";
+import SurveyApplication from "./components/Survey";
+import useFetchData from "./hooks/useFetchData";
 
 function App() {
   const [data, setData] = useState(null);
   const { fetchData } = useFetchData();
 
   const executeFetch = async () => {
-    await fetchData('/survey/', 'POST', { id: '68a8f5c85e6b02b365661338' }).then((_data) => setData(_data));
+    await fetchData("/survey/", "POST", {
+      id: "68a8f5c85e6b02b365661338",
+    }).then((_data) => setData(_data));
   };
 
   useEffect(() => {
@@ -18,13 +19,7 @@ function App() {
   return (
     <main className="grid">
       <div className="col-12 md:col-10 md:col-offset-1">
-        {data && (
-          <BrowserRouter>
-            <Routes>
-              <Route exact path="/" element={<SurveyApplication encuesta={data.encuesta} />} />
-            </Routes>
-          </BrowserRouter>
-        )}
+        {data && <SurveyApplication encuesta={data.encuesta} />}
       </div>
     </main>
   );
